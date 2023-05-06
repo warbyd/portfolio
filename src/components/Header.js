081978fb2115a33a1b5c4a2b15b573734d10f41e
warbyd <dalewwarburton@gmail.com>
2023-05-06 14:55:24 +0100

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css'
-import logo from './logo.jpg'; // Import the image file with the correct name
+import logo from './logo.png'; // Import the image file with the correct name
 
 const Header = () => {
   return (
-    <div>
-      <img src={logo} alt="Logo" className="logo" /> {/* Add the logo image */}
-      <nav>
-        <div className="logo-container"> {/* Add a container for the logo */}
-          <img src={logo} alt="Logo" className="logo" /> {/* Add the logo image */}
-        </div>
-        <ul>
-          <li><NavLink to="/portfolio">Home</NavLink></li>
-          <li><NavLink to="/cv">CV</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
-        </ul>
-      </nav>
-    </div>
+    <nav>
+      <div className="logo-container"> {/* Add a container for the logo */}<img src={logo} alt="Logo" className="logo" /> {/* Add the logo image */}
+      </div>
+      <ul>
+        <li><NavLink to="/portfolio">Home</NavLink></li>
+        <li><NavLink to="/cv">CV</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>
+    </nav>
   );
 }
 

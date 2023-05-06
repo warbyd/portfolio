7526d9d3f73d662cfbf8087cc97baffe4ffaa879
warbyd <dalewwarburton@gmail.com>
2023-05-06 07:46:00 +0100

    slight changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,20 @@ import logo from './logo.jpg'; // Import the image file with the correct name
 
 const Header = () => {
   return (
-    <nav>
-      <div className="logo-container"> {/* Add a container for the logo */}
-        <img src={logo} alt="Logo" className="logo" /> {/* Add the logo image */}
-      </div>
-      <ul>
-        <li><NavLink to="/portfolio">Home</NavLink></li>
-        <li><NavLink to="/cv">CV</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
-      </ul>
-    </nav>
+    <div>
+      <img src={logo} alt="Logo" className="logo" /> {/* Add the logo image */}
+      <nav>
+        <div className="logo-container"> {/* Add a container for the logo */}
+          <img src={logo} alt="Logo" className="logo" /> {/* Add the logo image */}
+        </div>
+        <ul>
+          <li><NavLink to="/portfolio">Home</NavLink></li>
+          <li><NavLink to="/cv">CV</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
